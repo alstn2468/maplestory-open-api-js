cf4989393b9874c2e18887b98a461c34412de52d
한예준 <102154824+sebanimm@users.noreply.github.com>
2024-01-08 10:32:03 +0900

    feat: 랭킹 URL, 랭킹 파라미터 타입, 랭킹 관련 함수 추가  (#5)
    * fix: 오타 수정 (HISOTRY -> HISTORY)
    
    * fix: 오타 수정 (STARFOCE -> STARFORCE)
    
    * fix: 오타 수정 (Paramter -> Parameter)
    
    * fix: 오타 수정 (decription -> description)
    
    * fix: 오타 수정 (UnionrBase -> UnionBase)
    
    * feat: 랭킹 URL 추가
    
    * feat: 랭킹 query parameter 타입 추가
    
    * feat: 랭킹 관련 함수 추가
    
    * feat: 배럴 파일에 랭킹 관련 export 추가
    
    * refactor: if문 제거
    
    * refactor: searchParams 타입, 기본값 수정
    
    * feat: excludeUndefinedFields 함수 추가
    
    * feat: RankingQueryParameter 타입에 인덱스 시그니처 추가
    
    * fix: 랭킹 관련 함수에 excludeUndefinedFields 함수 적용

---
+++ packages/maplestory-open-api-js/src/ranking.ts
@@ -0,0 +1,169 @@
+import { get } from './fetcher';
+import {
+  AchievementRankingQueryParameter,
+  DojangRankingQueryParameter,
+  GuildRankingQueryParameter,
+  OverallRankingQueryParameter,
+  TheSeedRankingQueryParameter,
+  UnionRankingQueryParameter,
+} from './types/params';
+import {
+  AchievementRanking,
+  DojangRanking,
+  GuildRanking,
+  OverallRanking,
+  TheSeedRanking,
+  UnionRanking,
+} from './types/ranking';
+import { RANKING_URLS } from './urls';
+import { excludeUndefinedFields, getPreviousFormattedDate } from './utils';
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param ocid 캐릭터 식별자
+ * @param character_class 직업 및 전직
+ * @param world_name 월드 명
+ * @param world_type 월드 타입 (0:일반, 1:리부트) (기본 값은 0이며, world_name 입력 시 미 반영)
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getOverallRanking({
+  ocid,
+  character_class,
+  world_name,
+  world_type = 0,
+  page,
+  date = getPreviousFormattedDate(),
+}: OverallRankingQueryParameter = {}) {
+  return get<OverallRanking>(RANKING_URLS.OVERALL, {
+    searchParams: excludeUndefinedFields<OverallRankingQueryParameter>({
+      ocid,
+      character_class,
+      world_name,
+      world_type,
+      page,
+      date,
+    }),
+  });
+}
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param ocid 캐릭터 식별자
+ * @param world_name 월드 명
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getUnionRanking({
+  ocid,
+  world_name,
+  page,
+  date = getPreviousFormattedDate(),
+}: UnionRankingQueryParameter = {}) {
+  return get<UnionRanking>(RANKING_URLS.UNION, {
+    searchParams: excludeUndefinedFields<UnionRankingQueryParameter>({
+      ocid,
+      world_name,
+      page,
+      date,
+    }),
+  });
+}
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param ranking_type 랭킹 타입 (0:주간 명성치, 1:플래그 레이스, 2:지하 수로)
+ * @param world_name 월드 명
+ * @param guild_name 길드 명
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getGuildRanking({
+  ranking_type,
+  world_name,
+  guild_name,
+  page,
+  date = getPreviousFormattedDate(),
+}: GuildRankingQueryParameter) {
+  return get<GuildRanking>(RANKING_URLS.GUILD, {
+    searchParams: excludeUndefinedFields<GuildRankingQueryParameter>({
+      ranking_type,
+      world_name,
+      guild_name,
+      page,
+      date,
+    }),
+  });
+}
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param difficulty 구간 (0:일반, 1:통달)
+ * @param ocid 캐릭터 식별자
+ * @param world_name 월드 명
+ * @param character_class 직업 및 전직
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getDojangRanking({
+  difficulty,
+  character_class,
+  ocid,
+  world_name,
+  page,
+  date = getPreviousFormattedDate(),
+}: DojangRankingQueryParameter) {
+  return get<DojangRanking>(RANKING_URLS.DOJANG, {
+    searchParams: excludeUndefinedFields<DojangRankingQueryParameter>({
+      difficulty,
+      character_class,
+      ocid,
+      world_name,
+      page,
+      date,
+    }),
+  });
+}
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param ocid 캐릭터 식별자
+ * @param world_name 월드 명
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getTheSeedRanking({
+  ocid,
+  world_name,
+  page,
+  date = getPreviousFormattedDate(),
+}: TheSeedRankingQueryParameter = {}) {
+  return get<TheSeedRanking>(RANKING_URLS.THESEED, {
+    searchParams: excludeUndefinedFields<TheSeedRankingQueryParameter>({
+      ocid,
+      world_name,
+      page,
+      date,
+    }),
+  });
+}
+
+/**
+ * @description 종합 랭킹 정보를 조회
+ * @param ocid 캐릭터 식별자
+ * @param page 페이지 번호
+ * @param date 조회 기준일 (KST, EXAMPLE: 2023-12-21)
+ */
+export function getAchievementRanking({
+  ocid,
+  page,
+  date = getPreviousFormattedDate(),
+}: AchievementRankingQueryParameter = {}) {
+  return get<AchievementRanking>(RANKING_URLS.ACHIEVEMENT, {
+    searchParams: excludeUndefinedFields<AchievementRankingQueryParameter>({
+      ocid,
+      page,
+      date,
+    }),
+  });
+}
